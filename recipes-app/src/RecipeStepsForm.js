d65e31d1ecb6237289a3367c566a545081d6a165
Zachary DeLuna <zach.deluna@gmail.com>
2018-10-07 13:00:46 -0500

    Updated step component to only show delete button on last step

--- recipes-app/src/RecipeStepsForm.js
+++ recipes-app/src/RecipeStepsForm.js
@@ -1,105 +1,107 @@
-import React, { Component } from "react";
-import { Route, Redirect } from "react-router-dom";
-import app from "./base";
-import Step from "./Step";
+//@format
 
+import React, {Component} from 'react';
+import {Route, Redirect} from 'react-router-dom';
+import app from './base';
+import Step from './Step';
 
 class RecipeStepsForm extends Component {
-	constructor(props){
-		super(props);
-		
-		this.state = {
-			value: '',
-			user: app.auth().currentUser,
-			numberOfSteps: 4,
-			stepForms: [
-			{ id: 1, value: "", visible: false},
-			{ id: 2, value: "", visible: false},
-			{ id: 3, value: "", visible: false},
-			{ id: 4, value: "", visible: true}
-			]
-		};
+    constructor(props) {
+        super(props);
 
-	}
+        this.state = {
+            value: '',
+            user: app.auth().currentUser,
+            numberOfSteps: 4,
+            stepForms: [
+                {id: 1, value: '', visible: false},
+                {id: 2, value: '', visible: false},
+                {id: 3, value: '', visible: false},
+                {id: 4, value: '', visible: true},
+            ],
+        };
+    }
 
-	addStep = (event) => {
-		
-		// Increase the number of steps listed in the state property
-		this.state.numberOfSteps++;
-		console.log(event);
+    addStep = event => {
+        // Set the last object of step forms to have a visible property of false
+        this.state.stepForms[this.state.numberOfSteps - 1]['visible'] = false;
 
-		// https://stackoverflow.com/questions/23966438/what-is-the-preferred-way-to-mutate-a-react-state
-		// Concatenate an array with stepForms that includes the new step
-		this.setState((state) => ({ stepForms: state.stepForms.concat(
-			{ id: this.state.numberOfSteps, value: ""}
-			)} 
-		))
+        // Increase the number of steps listed in the state property
+        this.state.numberOfSteps++;
 
-	}
+        // https://stackoverflow.com/questions/23966438/what-is-the-preferred-way-to-mutate-a-react-state
+        // Concatenate an array with stepForms that includes the new step
+        this.setState(state => ({
+            stepForms: state.stepForms.concat({
+                id: this.state.numberOfSteps,
+                value: '',
+                visible: true,
+            }),
+        }));
+    };
 
+    handleChange = (id, description) => {
+        this.state.stepForms[id - 1].value = description;
+        this.forceUpdate();
+    };
 
-	handleChange = (id, description) => {
+    handleSubmit = event => {
+        //alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
 
-		this.state.stepForms[id-1].value = description;
-		this.forceUpdate();
-		
-	}
+        updateDatabase(
+            this.state.stepForms,
+            this.state.user.uid,
+            this.props.dishId,
+        );
+    };
 
+    handleDeleteStep = id => {
+        console.log('Delete step' + id);
+        this.removeStep(id - 1);
+        this.state.numberOfSteps--;
+    };
 
-	handleSubmit = (event) => {
-		//alert('A name was submitted: ' + this.state.value);
-		event.preventDefault();
+    // https://stackoverflow.com/questions/29527385/removing-element-from-array-in-component-state
 
-		updateDatabase(this.state.stepForms, this.state.user.uid, this.props.dishId);
-	}
+    removeStep(id) {
+        this.setState({
+            stepForms: this.state.stepForms.filter((_, i) => i !== id),
+        });
+    }
 
-	handleDeleteStep = (id) => {
-		console.log("Delete step" + id);
-		this.removeStep(id - 1);
-		this.state.numberOfSteps--;
-
-	}
-
-	removeStep(id){
-		this.setState({
-			stepForms: this.state.stepForms.filter((_, i) => i !== id)
-		
-		});
-
-	}
-
-
-	render() {
-		return (
-			<div>
-				<h2>Add Recipe Steps Manually</h2>
-				<form onSubmit={this.handleSubmit}>
-					{this.state.stepForms.map(stepForm => 
-						<Step key={stepForm.id} value={stepForm.value} 
-							id={stepForm.id} 
-							onChange={this.handleChange} 
-							onClick={this.handleDeleteStep} 
-							onBlue={this.handleChange} 
-							deleteButton={stepForm.visible}
-						/>)}
-						<input type="submit" value="submit" />
-						<button type="button" onClick={this.addStep}>Add Step</button>
-				</form>
-			</div>
-		);
-	}
+    render() {
+        return (
+            <div>
+                <h2>Add Recipe Steps Manually</h2>
+                <form onSubmit={this.handleSubmit}>
+                    {this.state.stepForms.map(stepForm => (
+                        <Step
+                            key={stepForm.id}
+                            value={stepForm.value}
+                            id={stepForm.id}
+                            onChange={this.handleChange}
+                            onClick={this.handleDeleteStep}
+                            onBlue={this.handleChange}
+                            deleteButton={stepForm.visible}
+                        />
+                    ))}
+                    <input type="submit" value="submit" />
+                    <button type="button" onClick={this.addStep}>
+                        Add Step
+                    </button>
+                </form>
+            </div>
+        );
+    }
 }
 
-
-function updateDatabase(steps, uid, dishId)
-{
-	console.log("update database" + steps);
-	app.database().ref().child('/dishes/' + uid + '/' + dishId)
-		.update({steps: steps});
-
-
-
+function updateDatabase(steps, uid, dishId) {
+    console.log('update database' + steps);
+    app.database()
+        .ref()
+        .child('/dishes/' + uid + '/' + dishId)
+        .update({steps: steps});
 }
-		
+
 export default RecipeStepsForm;
-
--- recipes-app/src/Step.js
+++ recipes-app/src/Step.js
@@ -1,62 +1,57 @@
-import React, { Component } from "react";
-import { Route, Redirect } from "react-router-dom";
-import app from "./base";
-
+//@format
+import React, {Component} from 'react';
+import {Route, Redirect} from 'react-router-dom';
+import app from './base';
 
 class Step extends Component {
-	constructor(props){
-		super(props);
-		
-		this.state = {
-			description: props.description, 
-			user: app.auth().currentUser,
-			id: props.id,
-			deleteButtonVisible: false
-		};
+    constructor(props) {
+        super(props);
 
-	}
+        this.state = {
+            description: props.description,
+            user: app.auth().currentUser,
+            id: props.id,
+        };
+    }
 
-	updateInput = (newValue) => {
-		this.setState({value: newValue});
-		console.log(this.state.value);
-	}
+    updateInput = newValue => {
+        this.setState({value: newValue});
+    };
 
+    handleChange = event => {
+        this.setState({value: event.target.value}, () => {
+            // Update the array contained in the steps form component
+            // https://stackoverflow.com/questions/33088482/onchange-in-react-doesnt-capture-the-last-character-of-text
 
-	handleChange = (event) => {
-		this.setState({value: event.target.value}, () => {
+            // Use a asynchronous callback
+            this.props.onChange(this.state.id, this.state.value);
+        });
+    };
 
-			// Update the array contained in the steps form component
-			// https://stackoverflow.com/questions/33088482/onchange-in-react-doesnt-capture-the-last-character-of-text
+    handleDeleteStep = event => {
+        this.props.onClick(this.state.id);
+    };
 
-			// Use a asynchronous callback
-			this.props.onChange(this.state.id, this.state.value);
-
-			
-
-		});
-
-	}
-
-	handleDeleteStep = (event) => {
-		this.props.onClick(this.state.id);
-
-
-	}
-
-
-	render() {
-		return (
-			<div>
-				<label>
-					Step #{this.state.id}
-					<input type="text" value={this.state.description} 
-						onChange={this.handleChange} onBlur={() => this.updateInput(this.state.value)} />
-				</label>
-				{ this.props.deleteButton ? <button type="button" onClick={this.handleDeleteStep}>Delete</button> : null }
-			</div>
-		);
-	}
+    render() {
+        return (
+            <div>
+                <label>
+                    Step #{this.state.id}
+                    <input
+                        type="text"
+                        value={this.state.description}
+                        onChange={this.handleChange}
+                        onBlur={() => this.updateInput(this.state.value)}
+                    />
+                </label>
+                {this.props.deleteButton ? (
+                    <button type="button" onClick={this.handleDeleteStep}>
+                        Delete
+                    </button>
+                ) : null}
+            </div>
+        );
+    }
 }
-
 
 export default Step;
