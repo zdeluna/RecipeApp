929377b9ef2725b11931b76eb21dc85eb8315283
Zachary DeLuna <zach.deluna@gmail.com>
2018-10-15 17:48:38 -0500

    Updated getRecipeFromWebPage by passing in an array and filling it with the steps parsed from the url

---
+++ recipe_files/ads.js
@@ -0,0 +1,1 @@
+window=window||{};window.seabdvWlydHTyN=true;
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
+var request = require('request');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8111;
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
@@ -9,6 +10,7 @@ app.use(bodyParser.json());
 var admin = require('firebase-admin');
 
 var serviceAccount = require('./recipeapp-4bd8d-firebase-adminsdk-2x3ae-e33243e2f1.json');
+const cheerio = require('cheerio');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -29,4 +31,34 @@ app.post('/api/dish/:id/recipe', (req, res) => {
         .update({url: req.body.url});
 });
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+    var stepsArray = [];
+    getRecipeStepsFromWebPage(stepsArray);
+});
+
+function getRecipeStepsFromWebPage(stepsArray) {
+    console.log('in function');
+
+    var json = [];
+
+    const url =
+        'https://www.seriouseats.com/recipes/2013/06/grilled-skirt-steak-fajitas-food-lab-recipe.html';
+
+    request(url, function(error, response, html, stepsArray) {
+        if (error) return;
+        var $ = cheerio.load(html);
+
+        $('.recipe-procedure-text').filter(function(stepsArray) {
+            var stepNumber = 1;
+
+            var data = $(this);
+            var stepDescription = data.children().text();
+
+            var step = {id: 0, value: stepDescription};
+            stepsArray.push(step);
+        });
+    });
+    console.log('out of request');
+    console.log(stepsArray);
+}
