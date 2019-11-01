f958b211801ddc5ffbc1135367345a72f039a4b7
Zachary DeLuna <zach.deluna@gmail.com>
2019-10-31 19:28:35 -0500

    Add user type

--- graphql_server/schema/schema.js
+++ graphql_server/schema/schema.js
@@ -6,11 +6,17 @@ const {
     GraphQLInt,
     GraphQLSchema,
     GraphQLID,
+    GraphQLList,
 } = graphql;
 
 var dishes = [
-    {name: 'Risotto', cookingTime: '40 minutes', id: '1'},
-    {name: 'Tacos', cookingTime: '20 minutes', id: '2'},
+    {name: 'Risotto', cookingTime: '40 minutes', id: '1', uid: '1'},
+    {name: 'Tacos', cookingTime: '20 minutes', id: '2', uid: '2'},
+];
+
+var users = [
+    {email: 'zach.deluna@gmail.com', id: '1'},
+    {email: 'usertest@gmail.com', id: '2'},
 ];
 
 const DishType = new GraphQLObjectType({
@@ -19,6 +25,20 @@ const DishType = new GraphQLObjectType({
         id: {type: GraphQLID},
         name: {type: GraphQLString},
         cookingTime: {type: GraphQLString},
+        user: {
+            type: UserType,
+            resolve(parent, args) {
+                return _.find(users, {id: parent.uid});
+            },
+        },
+    }),
+});
+
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields: () => ({
+        id: {type: GraphQLID},
+        email: {type: GraphQLString},
     }),
 });
 
@@ -30,6 +50,19 @@ const RootQuery = new GraphQLObjectType({
             args: {id: {type: GraphQLID}},
             resolve(parent, args) {
                 return _.find(dishes, {id: args.id});
+            },
+        },
+        dishes: {
+            type: new GraphQLList(DishType),
+            resolve(parent, args) {
+                return dishes;
+            },
+        },
+        user: {
+            type: UserType,
+            args: {id: {type: GraphQLID}},
+            resolve(parent, args) {
+                return _.find(users, {id: args.id});
             },
         },
     },
