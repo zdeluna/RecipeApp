1ec8bf62935b0a36214bdcb1b8ceae4ee7489d03
Zachary DeLuna <zach.deluna@gmail.com>
2019-11-07 17:13:35 -0600

    Update add dish form to use mutation hook

--- client/src/components/AddDishForm.js
+++ client/src/components/AddDishForm.js
@@ -3,6 +3,56 @@ import app from '../base';
 import {Form, Button, FormGroup, Label, Input, Container} from 'reactstrap';
 import './AddDishForm.css';
 import API from '../utils/Api';
+import {ApolloClient} from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo';
+import {gql} from 'apollo-boost';
+import {graphql} from 'react-apollo';
+import {useMutation} from '@apollo/react-hooks';
+import {Query} from 'react-apollo';
+
+const ADD_DISH = gql`
+    mutation addDish($name: String!, $category: String!, $uid: ID!) {
+        addDish(name: $name, category: $category, uid: $uid) {
+            id
+            name
+        }
+    }
+`;
+
+function AddDish(props) {
+    let input = {value: ''};
+    const [addDish, {data}] = useMutation(ADD_DISH);
+
+    return (
+        <Container>
+            <Form
+                inline
+                onSubmit={e => {
+                    e.preventDefault();
+                    addDish({
+                        variables: {
+                            name: input.value,
+                            category: props.category,
+                            uid: props.uid,
+                        },
+                    });
+                    input.value = '';
+                }}>
+                <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                    <Label for="newDishInput">New Dish Name: </Label>
+                    <Input
+                        type="text"
+                        id="newDishInput"
+                        onChange={e => {
+                            input.value = e.target.value;
+                        }}
+                    />
+                </FormGroup>
+                <Button color="primary">Submit</Button>
+            </Form>
+        </Container>
+    );
+}
 
 class AddDishForm extends Component {
     constructor(props) {
@@ -19,18 +69,6 @@ class AddDishForm extends Component {
         this.setState({value: event.target.value});
     };
 
-    handleSubmit = event => {
-        event.preventDefault();
-
-        // Get the category of the dish from the query in the url
-
-        this.addNewDish(
-            this.state.user.uid,
-            this.state.value,
-            this.state.category,
-        );
-    };
-
     // Make a call to the api to handle parsing the recipe from the url
     addNewDish = async (userId, dishName, category) => {
         // prettier-ignore
@@ -39,29 +77,23 @@ class AddDishForm extends Component {
         newDish.name = dishName;
         newDish.category = category;
 
-        const api = new API();
+        /*
+		const api = new API();
+
         api.createDish(this.state.user.uid, newDish).then(response => {
             if (response.status === 201) this.props.onClick(response.data.id);
-        });
+		
+
+});*/
     };
 
     render() {
         return (
             <div>
-                <Container>
-                    <Form inline onSubmit={this.handleSubmit}>
-                        <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-                            <Label for="newDishInput">New Dish Name: </Label>
-                            <Input
-                                type="text"
-                                value={this.state.value}
-                                onChange={this.handleChange}
-                                id="newDishInput"
-                            />
-                        </FormGroup>
-                        <Button color="primary">Submit</Button>
-                    </Form>
-                </Container>
+                <AddDish
+                    category={this.state.category}
+                    uid={this.state.user.uid}
+                />
             </div>
         );
     }
