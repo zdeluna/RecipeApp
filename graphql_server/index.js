93a893f033e7eee61871a037ad9c3317f0fd3069
Zachary DeLuna <zach.deluna@gmail.com>
2020-01-29 15:51:51 -0600

    Update package.json

--- graphql_server/index.js
+++ graphql_server/index.js
@@ -1,27 +1,42 @@
-const {ApolloServer, gql} = require('apollo-server-cloud-functions');
 const typeDefs = require('./schema/schema.js');
 const resolvers = require('./resolvers');
 
 const DishAPI = require('./datasources/Dish');
+console.log('IN INDEX: ' + process.env.GRAPH_ENV);
+if (process.env.GRAPH_ENV == 'test') {
+    const {ApolloServer} = require('apollo-server');
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        dataSources: () => ({
+            dishAPI: new DishAPI(),
+        }),
+    });
 
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    dataSources: () => ({
-        dishAPI: new DishAPI(),
-    }),
-    playground: true,
-    introspection: true,
-    context: ({req, res}) => ({
-        headers: req.headers,
-        req,
-        res,
-    }),
-});
+    server.listen().then(({url}) => {
+        console.log(`GraphQL Server ready at ${url}`);
+    });
+} else {
+    const {ApolloServer} = require('apollo-server-cloud-functions');
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        dataSources: () => ({
+            dishAPI: new DishAPI(),
+        }),
+        playground: true,
+        introspection: true,
+        context: ({req, res}) => ({
+            headers: req.headers,
+            req,
+            res,
+        }),
+    });
 
-exports.handler = server.createHandler({
-    cors: {
-        origin: '*',
-        credentials: true,
-    },
-});
+    exports.handler = server.createHandler({
+        cors: {
+            origin: '*',
+            credentials: true,
+        },
+    });
+}
