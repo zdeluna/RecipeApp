d204f0c29a16c9c5113327d4d75943b74f229d5e
Zachary DeLuna <zach.deluna@gmail.com>
2020-01-30 16:20:04 -0600

    Separate parse functions from dish controller file

--- controllers/dish.js
+++ controllers/dish.js
@@ -3,6 +3,10 @@ const userModel = require('../models/user.js');
 const request = require('request');
 const cheerio = require('cheerio');
 const {sendErrorResponse} = require('./base.js');
+const {
+    getStepsFromWebPage,
+    getIngredientsFromWebPage,
+} = require('../utils/parseRecipe/parseRecipe.js');
 
 /**
  * Make a http request to the url and store the steps and ingredients in an object
@@ -41,45 +45,6 @@ const getRecipeStepsAndIngredientsFromWebPage = async url => {
         });
     });
 };
-
-/**
- * Check to see if the passed in text is a possible heading for steps
- * @param {String} text
- * @Return {boolean}
- */
-
-function checkForStepsHeading(text) {
-    // Remove semicolon
-    text = text.replace(/:/gi, '');
-    const acceptableStepsHeading = [
-        'instructions',
-        'directions',
-        'preparation',
-    ];
-
-    if (acceptableStepsHeading.indexOf(text) > -1) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-/**
- * Check to see if the passed in text is a possible heading for ingredients
- * @param {String} text
- * @Return {boolean}
- */
-
-function checkForIngredientsHeading(text) {
-    // Remove semicolon
-    text = text.replace(/:/gi, '');
-
-    const acceptableIngredientsHeading = ['ingredients', 'ingredients:'];
-
-    if (acceptableIngredientsHeading.indexOf(text) > -1) {
-        return true;
-    } else return false;
-}
 
 /**
  * Return True if ingredient is found in step, otherwise return False
@@ -209,60 +174,6 @@ const getIngredientsInSteps = async (steps, ingredients) => {
     return ingredientsInStepsArray;
 };
 
-/*
-* Return the html that contains "Ingredients" or "Steps/Instructions/Directions"
-* @param {String} $ - html to search
-* @param {String} heading - the type of heading to search for such "ingredients"
- * @Return {Promise containing the html}
- */
-
-const findHeading = async ($, heading) => {
-    return new Promise(async (resolve, reject) => {
-        let html = $('h1, h2, h3, h4, h5').filter(function() {
-            if (heading == 'ingredients') {
-                return checkForIngredientsHeading(
-                    $(this)
-                        .text()
-                        .trim()
-                        .toLowerCase(),
-                );
-            } else {
-                return checkForStepsHeading(
-                    $(this)
-                        .text()
-                        .trim()
-                        .toLowerCase(),
-                );
-            }
-        });
-
-        if (html == '') {
-            console.log('REJECT HEADING NOT FOUND');
-            reject();
-        }
-
-        resolve(html);
-    });
-};
-
-/**
- * Check to make sure list html is referring to the most nested list
- * @param {String} listHTML - The html of the contents after the "Instructions" or "Ingredients" heading
- * @Return {Promise}
- */
-
-const cleanList = async listHTML => {
-    return new Promise(async (resolve, reject) => {
-        /* Check to make sure we are searching through most nested ordered list or unordered list.
-		 * If we are not, then return the nested ordered or unordered list */
-
-        if (listHTML.find('ol, ul').children().length > 0) {
-            listHTML = listHTML.find('ol, ul');
-        }
-        resolve(listHTML);
-    });
-};
-
 /**
  * Remove the number labels in a text such as "1." or "1)"
  * @param {String} text - The text containing the number label
@@ -278,131 +189,6 @@ const removeNumberLabel = text => {
     newText = text.replace(/^\d+\)\s*/, '');
 
     return newText;
-};
-
-/**
- * Parse through a web page and store the steps of the recipe
- * @param {String} $ - Represents the html contents of a page
- * @Return {Promise}
- */
-
-const getStepsFromWebPage = async $ => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            let stepsArray = [];
-            // Find the heading that contains instruction
-            let headingHTML = await findHeading($, 'steps');
-            console.log('HEADING: ' + headingHTML);
-            stepsHTML = await findList(headingHTML);
-            stepsHTML = await cleanList(stepsHTML);
-            stepsHTML
-                .children('li') // Find the children of the list
-                // Iterate through each child element and store the text of the element and add it to the array
-                .each(function(index, element) {
-                    let stepDescription = $(this)
-                        .text()
-                        .trim();
-                    /* Remove step number if text already contained them.*/
-                    stepDescription = removeNumberLabel(stepDescription);
-
-                    let step = {id: stepsArray.length, value: stepDescription};
-                    stepsArray.push(step);
-                });
-            resolve(stepsArray);
-        } catch (error) {
-            reject({
-                statusCode: 422,
-                msg: 'CANNOT_RETRIEVE_STEPS',
-            });
-        }
-    });
-};
-
-/**
- * Search through DOM for an unordered or orderlist list but investigating current node and traversing down the DOM tree if not found
- * @Return {Promise}
- */
-
-const findList = async headingNode => {
-    return new Promise(async (resolve, reject) => {
-        let listElements = '';
-        let listHTML = '';
-
-        // First check the children of the heading node for text
-        listElements = headingNode.children().text();
-
-        if (listElements != '') {
-            resolve(headingNode);
-        }
-
-        // First check the next sibling of heading node for text
-        listElements = headingNode.next().text();
-
-        if (listElements != '') {
-            return resolve(headingNode.next());
-        }
-
-        listElements = headingNode
-            .parent()
-            .next()
-            .text();
-
-        if (listElements != '') {
-            resolve(headingNode.parent().next());
-        }
-
-        // Otherwise start from the parent of the heading node and traverse up to find an ordered or unordered list
-        let searchHTML = headingNode;
-        console.log('SEARCHING THROUGH TRAVERSING TREE');
-        while (listElements == '') {
-            searchHTML = searchHTML.parent();
-            listElements = searchHTML.find('ul, ol');
-
-            //If we make it to the top level without finding it, send back a reject
-            if (searchHTML.initialize) {
-                reject();
-            }
-        }
-        resolve(listElements);
-    });
-};
-
-/**
- * Parse through a web page and store the ingredients of the recipe
- * @param {String} $ - Represents the html contents of a page
- * @Return {Promise}
- */
-
-const getIngredientsFromWebPage = async $ => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            let ingredientsArray = [];
-
-            let headingHTML = await findHeading($, 'ingredients');
-            ingredientsHTML = await findList(headingHTML);
-            ingredientsHTML = await cleanList(ingredientsHTML);
-
-            ingredientsHTML
-                .children() // Find the children of the list
-                // Iterate through each child element and store the text of the element and add it to the array
-                .each(function(index, element) {
-                    let ingredientDescription = $(this)
-                        .text()
-                        .trim();
-                    let ingredient = {
-                        id: ingredientsArray.length,
-                        value: ingredientDescription,
-                    };
-                    ingredientsArray.push(ingredient);
-                });
-            resolve(ingredientsArray);
-        } catch (error) {
-            reject({
-                statusCode: 422,
-                msg: 'CANNOT_RETRIEVE_INGREDIENTS',
-            });
-        }
-    });
 };
 
 exports.createDish = async (req, res) => {
