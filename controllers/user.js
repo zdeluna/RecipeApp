cd685275a0288d1ad95811d4e7f2a29cbc658bef
Zachary DeLuna <zach.deluna@gmail.com>
2020-04-10 16:27:53 -0500

    Create token in create user controller function

--- controllers/user.js
+++ controllers/user.js
@@ -1,18 +1,28 @@
-'use strict';
-const {sendErrorResponse} = require('./base.js');
-const userModel = require('../models/user.js');
-const {getConnection} = require('../dbconfig.js');
+"use strict";
+const { sendErrorResponse } = require("./base.js");
+const userModel = require("../models/user.js");
+const firebase = require("firebase-admin");
 
 const createUser = async (req, res) => {
     try {
-        const pool = await req.app.get('pool');
+        console.log("create user");
+        const pool = await req.app.get("pool");
         const email = req.body.email;
-        const googleId = req.body.googleId;
-        const user = await userModel.createUser(pool, googleId, email);
-        res.status(200).json('OK');
+        const password = req.body.password;
+
+        const user = await firebase
+            .auth()
+            .createUser({ email: email, password: password });
+
+        let token = await firebase.auth().createCustomToken(user.uid);
+
+        await userModel.createUser(pool, googleId, email);
+
+        res.status(200).json({ token: token });
     } catch (error) {
-        return Error({statusCode: 422, msg: error.message});
+        console.log(error);
+        return Error({ statusCode: 422, msg: error.message });
     }
 };
 
-module.exports = {createUser};
+module.exports = { createUser };
