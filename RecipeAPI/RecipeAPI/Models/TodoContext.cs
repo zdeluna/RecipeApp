4e0f3b2e56e19d3d256b32224a8dd49b6ffc3d61
Zachary DeLuna <zach.deluna@gmail.com>
2020-11-14 17:03:10 -0600

    Add API/ASP.net files

---
+++ RecipeAPI/RecipeAPI/Models/TodoContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+namespace RecipeAPI.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options): base(options)
+        {
+        }
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
