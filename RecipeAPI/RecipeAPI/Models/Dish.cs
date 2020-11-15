073bfad2e30487a1b5eef28cabca65ab10459079
Zachary DeLuna <zach.deluna@gmail.com>
2020-11-15 09:08:26 -0600

    Add dish db context and model

---
+++ RecipeAPI/RecipeAPI/Models/Dish.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace RecipeAPI.Models
+{
+    public class Dish
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public long category { get; set; }
+    }
+}
---
+++ RecipeAPI/RecipeAPI/Models/DishContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace RecipeAPI.Models
+{
+    public class DishContext : DbContext
+    {
+        public DishContext(DbContextOptions<DishContext> options) : base(options)
+        {
+        }
+        public DbSet<Dish> Dishes { get; set; }
+    }
+}
