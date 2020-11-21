4cf7415957a641b44f3ea1653f4839cd5bd94c54
Zachary DeLuna <zach.deluna@gmail.com>
2020-11-21 13:28:36 -0600

    Add Login controller

---
+++ RecipeAPI/RecipeAPI/Controllers/LoginController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IdentityModel.Tokens.Jwt;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using RecipeAPI.Models;
+
+
+namespace RecipeAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        private List<User> appUsers = new List<User>
+        {
+            new User { UserName = "testuser", Password = "1234" }
+        };
+
+
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Login([FromBody]User login)
+        {
+            IActionResult response = Unauthorized();
+            User user = AuthenticateUser(login);
+            if (user != null)
+            {
+                var tokenString = GenerateJWTToken(user);
+                response = Ok(new
+                {
+                    token = tokenString
+                });
+            }
+            return response;
+        }
+    }
+}
--- RecipeAPI/RecipeAPI/Startup.cs
+++ RecipeAPI/RecipeAPI/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using RecipeAPI.Models;
 
 namespace RecipeAPI
@@ -31,6 +32,28 @@ namespace RecipeAPI
             services.AddDbContext<DishContext>(opt => opt.UseInMemoryDatabase("RecipeAPI"));
             services.AddDbContext<UserContext>(opt => opt.UseInMemoryDatabase("RecipeAPI"));
             services.AddControllers();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration["Jwt:Issuer"],
+                    ValidAudience = Configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:SecretKey"])),
+                    ClockSkew = TimeSpan.Zero
+                };
+
+            });
+
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +68,8 @@ namespace RecipeAPI
 
             app.UseRouting();
 
+     
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
