8a1500c7d28df861cb1a71b0ec3589db02f3a6cd
Zachary DeLuna <zach.deluna@gmail.com>
2020-11-29 10:40:59 -0600

    Add patch to dish controller

--- RecipeAPI/RecipeAPI/MappingProfile/AutoMapperProfile.cs
+++ RecipeAPI/RecipeAPI/MappingProfile/AutoMapperProfile.cs
@@ -2,19 +2,24 @@
 using AutoMapper;
 using RecipeAPI.Models;
 using System.Linq;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.JsonPatch.Operations;
+
 namespace RecipeAPI.MappingProfile
 {
     public class AutoMapperProfile : Profile
     {
         public AutoMapperProfile()
         {
-            CreateMap<History, HistoryResponse>();
-            CreateMap<HistoryResponse, History>();
+            
             CreateMap<User, UserResponse>();
             CreateMap<Dish, DishResponse>()
                 .ForMember(dest => dest.History, opt =>
                     opt.MapFrom(src => src.History.Select(x => x.Date).ToList()));
-
+            CreateMap<UpdateDishRequest, Dish>();
+            CreateMap<Dish, UpdateDishRequest>();
+            //CreateMap<JsonPatchDocument<Dish>, JsonPatchDocument<UpdateDishRequest>>();
+            //CreateMap<Operation<Dish>, Operation<UpdateDishRequest>>();
 
 
         }
---
+++ RecipeAPI/RecipeAPI/Models/UpdateDishRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+namespace RecipeAPI.Models
+{
+    public class UpdateDishRequest
+    {
+        public string Name { get; set; }
+        public long Category { get; set; }
+        public string CookingTime { get; set; }
+        public string LastMade { get; set; }
+        public string Notes { get; set; }
+        public string Url { get; set; }
+
+        public List<string> History { get; set; }
+    }
+}
