112a530127c6dacfa4243d01b400430238955301
Zachary DeLuna <zach.deluna@gmail.com>
2020-12-08 19:18:43 -0600

    Add initial migration

--- RecipeAPI/RecipeAPI/Startup.cs
+++ RecipeAPI/RecipeAPI/Startup.cs
@@ -32,7 +32,10 @@ namespace RecipeAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DatabaseContext>(opt => opt.UseInMemoryDatabase("RecipeAPI"));
+            //services.AddDbContext<DatabaseContext>(opt => opt.UseInMemoryDatabase("RecipeAPI"));
+
+            services.AddDbContext<DatabaseContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("GCloudDbContext")));
+
             services.AddControllers().AddNewtonsoftJson();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
